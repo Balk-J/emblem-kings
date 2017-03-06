ed2af927b3ebb071f092fb71c0ca98564c54b247
Balk <balk.624@gmail.com>
2017-03-05 20:13:04 -0600

    Version 0.6 changes

---
+++ Emblem Kings/common/defines/fe_defines.lua
@@ -0,0 +1,6 @@
+NDefines.NCharacter.PORTRAIT_ADULT_MALE_AGE_THRESHOLD = 15			-- Male child to adult age portrait switch
+NDefines.NCharacter.PORTRAIT_ADULT_FEMALE_AGE_THRESHOLD = 15		-- Female child to adult age portrait switch
+NDefines.NCharacter.AGE_OF_ADULTHOOD_MALE = 15						-- Male can rule at this age. 
+NDefines.NCharacter.AGE_OF_ADULTHOOD_FEMALE = 15					-- Female can rule at this age.
+NDefines.NCharacter.AGE_OF_MARRIAGE_MALE = 15					-- Male allowed to marry at this age.
+NDefines.NCharacter.AGE_OF_MARRIAGE_FEMALE = 15					-- Female allowed to marry at this age.
